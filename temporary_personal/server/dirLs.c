7d97cc1e446c2c431d404b313a9693f9c5df54da
Lionel1999 <980980068@qq.com>
2022-03-19 09:51:09 +0800

    trash3

--- temporary_personal/server/dirLs.c
+++ temporary_personal/server/dirLs.c
@@ -1,19 +1,83 @@
+#include <func.h>
+#define BUFFER_SIZE 4096
 //接受客户端ls命令列出目录文件 文件名加空格
 //高哥
-int dirLs(char *specific, char *buf);
 
-int main()
-{
-    char specific;
-}
 int dirLs(char *specific, char *buf)
 {
     //不应处理：不讲武德到参数不合法的情况(输错了输成两段等情况)暂不处理（存在将错就错的情况）
-    //  ？？？是否要考虑到将错就错的情况-->第一行打印 即将打印的-->"{[目录/文件]的[绝对路径]}:"
-    //      ↑暂定处理
+    //
+    //不会处理：
+    //      1. 参数的合理性：如果填入路径在server之外，则应报错
+    //      这样想下去好像没有尽头了.....好复杂
+    //
+    //
+    //
+    //  ？？？是否要考虑到将错就错的情况，给个提示？
+    //          -->第一行打印 即将打印的-->"{[目录/文件]的[绝对路径]}:"
+    //      ↑暂定处理（注释掉了，还是先不处理了）
+    //      ↓暂未处理
+    //      参数为文件的情况暂未处理
+    //
     //--------则specific应恰为一个绝对路径或相对路径
     //
     //应处理：
-    //      1. 路径不存在-->perror
+    //      1. 路径不存在-->print error
     //      2. 输入的是文件-->则打印该文件名
+
+    bzero(buf, BUFFER_SIZE);
+
+    ////此处可调用dirPwd打印当前路径
+
+    // int ret = chidir(specific); //可填dirPwd返回值
+    // if (ret == -1)
+    //{
+    //     // strcpy(buf, "ERROR: No such directory, please check your input\n");
+    //     return -1; //-1: ERROR: No such directory, please check your input\n
+    //     //或许应该返回errno???
+    // }
+
+    //路径不存在与打开路径失败剥离开
+    DIR *dirp = opendir(specific);
+    if (dirp == NULL)
+    {
+        // strcpy(buf, "ERROR: failed to opendir, please check your input\n");
+        return -2; //-2:ERROR: failed to opendir, please check your input
+        //或许应该返回errno???
+    }
+
+    struct dirent *pdirent;
+
+    while ((pdirent = readdir(dirp)))
+    {
+        //暂未处理ls的参数是文件的情况
+        if (strcmp(pdirent->d_name, ".") == 0 || strcmp(pdirent->d_name, "..") == 0)
+        {
+            continue;
+        }
+
+        strcat(buf, pdirent->d_name);
+        strcat(buf, "  "); //两个空格
+    }
+    //读取完毕或报错都会走到这一行
+
+    //如果最后一个是空格，删掉
+    int len = strlen(buf);
+    while (buf[len - 1] == ' ')
+    {
+        buf[len - 1] = 0;
+        len--;
+    }
+    // buf末尾没有换行，如有需要可添加
+    return 0;
+}
+
+int main()
+{
+    char specific[] = ".";
+    char buf[BUFFER_SIZE] = {0};
+    int ret = dirLs(specific, buf);
+    printf("%s-end\n", buf);
+
+    return 0;
 }
--- temporary_personal/server/dirPwd.c
+++ temporary_personal/server/dirPwd.c
@@ -1,3 +1,46 @@
+#include <func.h>
+#define BUFFER_SIZE 4096
+
 //接收客户端列出文件路径的命令，起始路径应为server根目录
 //高哥
-int dirPwd(char *specific, char *buf);
+// int dirPwd(char *specific, char *buf)
+int dirPwd(char *buf)
+{
+    ////为啥会有specific参数,假定为正在浏览的路径
+    // bzero(buf, BUFFER_SIZE);
+
+    // int ret = chdir(specific);
+    // if (ret == -1)
+    //{
+    //     strcpy(buf, "ERROR: No such file, please check your input\n");
+    //     return -1; //-1: ERROR: No such file, please check your input
+    //     //或许应该返回errno???
+    // }
+
+    char *pret = getcwd(buf, BUFFER_SIZE);
+    if (pret == NULL)
+    {
+        // strcpy(buf, "ERROR: failed to get absolute path");
+        return -2; //-2: ERROR: failed to get absolute path
+        //或许应该返回errno???
+    }
+    else
+    {
+        //处理起始路径
+        // 当前处理方法如服务端根目录改变则需要维护
+        // buffer = buffer + 偏移量;仅示意，不是这样写
+        // memmove(buffer, buffer+偏移量, BUFFER_SIZE-偏移量);
+        return 0;
+    }
+}
+
+int main()
+{
+    // char specific[] = ".";
+    char buf[BUFFER_SIZE] = {0};
+    int ret = dirPwd(buf);
+    // int ret = chdir(".");
+    // ERROR_CHECK(ret, -1, "chdir");
+    printf("%s-end\n", buf);
+    return 0;
+}
