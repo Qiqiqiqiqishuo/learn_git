12298f61b823540632b2fe1e97de5aa98fcd359d
Lionel1999 <980980068@qq.com>
2022-03-22 10:20:04 +0800

    dumb execute_sql_output design! G!

---
+++ netDisk3.0/server/manipulate_mysql.c
@@ -0,0 +1,54 @@
+#include "manipulate_mysql.h"
+
+int execute_sql_output(MYSQL *mysql, const char *sql)
+{
+    int sqlRet;
+    sqlRet = mysql_query(mysql, sql);
+    if (sqlRet != 0)
+    {
+        printf("Error: %s\n", mysql_error(mysql));
+        return -1;
+    }
+    printf("suc exec \"%s\"\n", sql);
+
+    MYSQL_RES *result = mysql_store_result(mysql);
+    if (result == NULL)
+    {
+        printf("Error: %s\n", mysql_error(mysql));
+        return -1;
+    }
+    printf("the row number = %lld\n", mysql_num_rows(result));
+
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row(result)))
+    {
+        for (int i = 0; (unsigned int)i < mysql_num_fields(result); i++)
+        {
+            printf("%s\t", row[i]);
+        }
+        printf("\n");
+    }
+    mysql_free_result(result);
+    printf("suc fetch all result\n");
+
+    return 0;
+}
+
+int connect_db(MYSQL *mysql, const char *host, const char *user, const char *passwd, const char *db, unsigned int port, const char *unix_socket, unsigned long client_flag)
+{
+    MYSQL *mysqlRet = mysql_real_connect(mysql, host, user, passwd, db, port, unix_socket, client_flag);
+    if (mysqlRet != NULL)
+    {
+        printf("mysql = %p, mysqlRet = %p\n", mysql, mysqlRet);
+        //打印指针用%p
+
+        printf("connect to DB \"%s\" succeed!\n", db);
+        return 0;
+    }
+    else
+    {
+        printf("Error: %s\n", mysql_error(mysql));
+
+        return -1;
+    }
+}
---
+++ netDisk3.0/server/manipulate_mysql.h
@@ -0,0 +1,17 @@
+#include <func.h>
+
+//  ----------初始化MYSQL对象，连接数据库----------
+//  1. MYSQL *mysql = mysql_init(NULL);
+//  2. ret = connect_db(xxx);
+//
+//  ----------开始具体操作并返回结果-------------
+//  ----------可能存在设计失误需要把 "操作并返回结果" 拆分成 "操作" 与 "各类型的返回结果"
+//  3. strcpy(sql, 命令字符串，注意加单引号);
+//  4. ret = execute_sql_output(db, sql);
+//  ----------具体操作结束-----------------------
+//
+//  ----------关闭数据库-------------------------
+//  5. mysql_close(db);
+
+int execute_sql_output(MYSQL *mysql, const char *sql);
+int connect_db(MYSQL *mysql, const char *host, const char *user, const char *passwd, const char *db, unsigned int port, const char *unix_socket, unsigned long client_flag);
